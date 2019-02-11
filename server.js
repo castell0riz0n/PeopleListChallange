e8b23a800b480b82acad94bc523ff750be14a0e5
Ali Khorsand <alikhorsand.work@outlook.com>
2019-02-11 19:56:54 +0330

    heroku release

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+let env = process.env.NODE_ENV || 'development';
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
+
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/people-list-challenge'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/people-list-challenge/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
